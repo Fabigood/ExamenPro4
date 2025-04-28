8aa9a95e93c287cc55a0356b05abb6adf3b2ff52
Fabigood <fiallosfabio@gmail.com>
2025-04-28 07:47:42 -0500

    clase reserva con annotation

--- ExaPro4/Models/Reserva.cs
+++ ExaPro4/Models/Reserva.cs
@@ -1,7 +1,20 @@
-﻿namespace ExaPro4.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExaPro4.Models
 {
     public class Reserva
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+
+        public DateTime FechaEntrada { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+
+        public DateTime FechaSalida { get; set; }
+
     }
 }
